263db9a86fd504648aec84084cf7260c494d5c13
Tom Postler <tom@postler.me>
2022-02-27 14:16:30 -0600

    discord dependency and bot token

--- src/po/Options.cs
+++ src/po/Options.cs
@@ -2,6 +2,10 @@
 {
     public static class Options
     {
+        public sealed class Discord
+        {
+            public string BotToken { get; set; }
+        }
         public sealed class Sql
         {
             public string ConnectionString { get; set; }
