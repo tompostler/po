678d5d1141c4cc7da27cabe1dda6031226609899
Tom Postler <tom@postler.me>
2022-02-27 12:30:47 -0600

    add root and no-self-contained

---
+++ src/po/Controllers/RootLevelController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace po.Controllers
+{
+    [ApiController]
+    public class RootLevelController : ControllerBase
+    {
+        private static ulong rootSequence = 0;
+
+        [HttpGet("/")]
+        public IActionResult Root()
+        {
+            return this.Ok(++rootSequence);
+        }
+
+        [HttpGet("/robots933456.txt")]
+        public IActionResult AzureAppServiceRobotsTxt()
+        {
+            // https://github.com/MicrosoftDocs/azure-docs/blob/f71ba01af68bd4859cecce515e7eeeab2d8dd298/includes/app-service-web-configure-robots933456.md
+            return this.NoContent();
+        }
+    }
+}
