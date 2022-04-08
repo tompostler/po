49e691f79ed22c76f1f392d6dc31017d7e519dba
Tom Postler <tom@postler.me>
2022-04-07 21:21:28 -0500

    and include the data migration

---
+++ src/po/Migrations/20220408022014_RemoveSlashCommandGuildId.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace po.Migrations
+{
+    public partial class RemoveSlashCommandGuildId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SlashCommandChannels",
+                table: "SlashCommandChannels");
+
+            migrationBuilder.DropColumn(
+                name: "GuildId",
+                table: "SlashCommandChannels");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SlashCommandChannels",
+                table: "SlashCommandChannels",
+                columns: new[] { "SlashCommandName", "ChannelId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_SlashCommandChannels",
+                table: "SlashCommandChannels");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "GuildId",
+                table: "SlashCommandChannels",
+                type: "decimal(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_SlashCommandChannels",
+                table: "SlashCommandChannels",
+                columns: new[] { "SlashCommandName", "GuildId", "ChannelId" });
+        }
+    }
+}
