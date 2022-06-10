709ed81715c628602b16eea4fa15f75a83004068
Tom Postler <tom@postler.me>
2022-06-09 20:35:38 -0500

    and include the migration...

---
+++ src/po/Migrations/20220610013352_ScheduledBlobContainerName.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace po.Migrations
+{
+    public partial class ScheduledBlobContainerName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContainerName",
+                table: "ScheduledBlobs",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContainerName",
+                table: "ScheduledBlobs");
+        }
+    }
+}
