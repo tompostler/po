aebf3ef04f06019f7f1f36dc86f1f7ae7d263361
Tom Postler <tom@postler.me>
2022-10-21 08:16:20 -0500

    fix sentinals

--- src/po/Utilities/Sentinals.cs
+++ src/po/Utilities/Sentinals.cs
@@ -13,20 +13,32 @@ namespace po.Utilities
         {
             private readonly SemaphoreSlim semaphore;
             private readonly Task task;
+
             public SentinalImplementation()
             {
                 this.semaphore = new SemaphoreSlim(0, 1);
                 this.task = this.semaphore.WaitAsync();
             }
+
             /// <summary>
             /// Should only be called once per sentinal.
             /// Used to release any waiting to let the startup process continue.
             /// </summary>
-            public void SignalCompletion() => _ = this.semaphore.Release(1);
+            public void SignalCompletion()
+            {
+                // When used in a handler, it's possible that this could be called more than once if the handler fires multiple times
+                // So only release the first time
+                if (this.semaphore.CurrentCount == 0)
+                {
+                    _ = this.semaphore.Release(1);
+                }
+            }
+
             /// <summary>
             /// A non-async way to check if we're ready before deciding to wait for completion.
             /// </summary>
             public bool IsReady => this.semaphore.CurrentCount > 0;
+
             /// <summary>
             /// Await this if you need to wait for the sentinal.
             /// If the cancellation token is cancelled, then what you were waiting for will not be ready.
@@ -50,24 +62,34 @@ namespace po.Utilities
             private readonly SemaphoreSlim semaphore;
             private readonly Task task;
             private T waitingFor;
+
             public SentinalImplementation()
             {
                 this.semaphore = new SemaphoreSlim(0, 1);
                 this.task = this.semaphore.WaitAsync();
             }
+
             /// <summary>
             /// Should only be called once per sentinal.
             /// Used to release any waiting to let the startup process continue.
             /// </summary>
             public void SignalCompletion(T thingThatIsNowReady)
             {
-                _ = this.semaphore.Release(1);
+                // When used in a handler, it's possible that this could be called more than once if the handler fires multiple times
+                // So only release the first time
+                if (this.semaphore.CurrentCount == 0)
+                {
+                    _ = this.semaphore.Release(1);
+                }
+
                 this.waitingFor = thingThatIsNowReady;
             }
+
             /// <summary>
             /// A non-async way to check if we're ready before deciding to wait for completion.
             /// </summary>
             public bool IsReady => this.semaphore.CurrentCount > 0;
+
             /// <summary>
             /// Await this if you need to wait for the sentinal and need something from it.
             /// </summary>
